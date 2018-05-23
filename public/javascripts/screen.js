d386adc82d2c715bf9687283f2b9552405c084a1
ARGONATH\Diego <dswanderley@gmail.com>
2018-05-23 11:34:48 +0100

    ADD: List of images in gallery

--- public/javascripts/screen.js
+++ public/javascripts/screen.js
@@ -1,6 +1,7 @@
 // JavaScript source code
 
 var urlBase = "";
+var galleryList = [];
 
 function grayscale() {
 
@@ -30,14 +31,15 @@ function grayscale() {
 
 
 function loadGallery() {
+    /** @description Load Gallery of images
+     */
 
-
+    // Load Gallery Div
     var gallery = $('#gallery');
-
-    el_ul = jQuery('<ul/>', {
+    // Create gallery ul - unordered list
+    var el_ul = jQuery('<ul/>', {
         class: 'galery-img'
     });
-
     // Ajax call
     $.ajax(
         {
@@ -48,36 +50,53 @@ function loadGallery() {
             cache: false,
             async: true,
             success: function (data) {
-
+                // reset List of images in gallery
+                galleryList = [];
                 i = 0;
-                console.log(data.length);
+                // Read images in gallery folder
                 data.forEach(file => {
-
+                    // Define image ID
                     im_id = 'g_img_' + i;
-                    
+                    // Create each image element - list item
                     el_ul.append(getGalleryEl(im_id, file));
+                    // Add filename to gallery list
+                    galleryList.push(file);
                     i += 1;
                 });
-
+                // Add list to gallery
                 gallery.append(el_ul);
             }
         });
 }
 
 function getGalleryEl(id, img) {
-    
-    el_l1 = jQuery('<li/>', {
+    /** @description Get image element for the gallery
+      * @param {string} g_img id
+      * @param {string} image name
+      * @return {jQuery} list item
+     */
+
+    // Create list item
+    el_li = jQuery('<li/>', {
         class: 'galery-img'
     });
-
+    // Create image element
     el_img = jQuery('<img/>', {
         class: 'gallery-thumb',
         id: id,
         height: '64px',
         src: 'gallery/' + img
     });
+    // Add image to list item
+    el_li.append(el_img);
 
-    el_l1.append(el_img);
-
-    return el_l1;
+    return el_li;
 }
+
+function setImage(src) {
+    /** @description Set image src
+      * @param {string} image src
+     */
+    $('#img-proc')[0].src = src
+}
+
